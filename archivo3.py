33172169625bae7cb131dc61847fa7ec0543e2f8
waltercardona <wacardona10@gmail.com>
2024-07-08 11:38:24 -0500

    se trabjo con el git stash

--- archivo3.py
+++ archivo3.py
@@ -1,1 +1,5 @@
-print("archivo 4")
+print("archivo 4, vamos a prtacticar con el git stahs")
+
+print('el git stats no ssirve para hacer commit de forma temporal en nuestro propio states sin necesidad de enviarlo al state')
+print('esto es lo que estaria mal , y lo vamos a stahs para pasarnos de rama y asi asi seguir en otra ocacion con lo que se venia hacuendo')
+print('despues de hacer lo que se nos pidiera en otra rama podemos continuar, lo restauramos con git stash pop')
